ce50cec1ef8f1dcd4fe49d2aaf33e2140da2209e
yonasshah <tup36693@temple.edu>
2025-10-13 20:49:41 -0400

    config

--- movie_roulette/project_config/wsgi.py
+++ movie_roulette/project_config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roulette.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_config.settings')
 
 application = get_wsgi_application()
