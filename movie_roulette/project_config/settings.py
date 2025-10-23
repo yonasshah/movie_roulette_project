08dfb7fed75bd9dc588b857324aa82e61c40ccee
yonasshah <tup36693@temple.edu>
2025-10-23 03:41:54 -0400

    database integration for deployment

--- movie_roulette/project_config/settings.py
+++ movie_roulette/project_config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,10 +52,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project_config.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this default value with your local development DB URL if needed,
+        # otherwise Render's DATABASE_URL environment variable will be used automatically.
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600 # Optional: Number of seconds database connections should persist
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
