55b2015ee39b591ab77c7a09e8f7abaa7a9d0a5f
yonasshah <tup36693@temple.edu>
2025-10-25 01:08:12 -0400

    Import added

--- movie_roulette/users/models.py
+++ movie_roulette/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from PIL import Image
+from PIL import Image, ExifTags
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
